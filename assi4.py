9efb36eb3a84fb7088c9abc682965d6f85adaf2d
vikas3799 <79507577+vikas3799@users.noreply.github.com>
2022-10-23 22:28:29 +0530

    Add files via upload

---
+++ assi4.py
@@ -0,0 +1,13 @@
+def push(s,n):
+    s.append(n)
+    return
+def pop(a):
+    if len==0:
+        print("Stack is Empty")
+    else:
+        d = s[len(s)-1]
+        del s[len(s)-1]
+        return d
+stack=[]
+print(push(stack,11))
+print(push(stack,22))
